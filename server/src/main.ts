ed867a5394e1bbc9cc0982c1aac0bda083b3df26
Mr_Adrs <adrshmishra018@gmail.com>
2026-02-22 19:14:59 +0530

    deployed over railway

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:5173", "https://apius.reqbin.com/api/v1","https://vercel.com"],
+    origin: ["http://localhost:5173", "https://apius.reqbin.com/api/v1","https://vercel.com","https://mini-order-system.vercel.app"],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
